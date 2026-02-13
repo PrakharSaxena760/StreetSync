a16988d820832d0eb7a1026d4295487ac3488211
Aayush <chandayush005@gmail.com>
2026-02-13 13:32:32 +0530

    add axios file

---
+++ frontend/src/api/user.auth.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const registerUser = async function (data) {
+  try {
+    const res = await axios.post(`/api/users/register`, data, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+    return {
+      status: res.status,
+      data: res.data,
+      error: null,
+    };
+  } catch (error) {
+    return {
+      status: error.response?.status || 500,
+      data: null,
+      error: error.response?.data || "something went wrong",
+    };
+  }
+};
+
+const loginUser = async function (data) {
+  try {
+    const res = await axios.post(`/api/users/login`, data, {
+      // using axios here and not api since have to use vite's proxy here, else the cookie is not being set in the browser
+      headers: { "Content-Type": "application/json" },
+      withCredentials: true,
+    });
+    return {
+      status: res.status,
+      data: res.data,
+      error: null,
+    };
+  } catch (error) {
+    return {
+      status: error.response?.status || 500,
+      data: null,
+      error: error.response?.data || "something went wrong",
+    };
+  }
+};
+
+
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  // have to send request to backend via a proxy since the cookies are not being set in cross origin
+   server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/api/v1", //  backend server
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""), // removes the extra /api
+      },
+    },
+  },
   plugins: [react(),tailwindcss()],
 })
